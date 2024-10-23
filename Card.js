5bbb4191c28610b93fef1fa4697eff4e7a4e7f88
Srivastav <138690746+srivastav04@users.noreply.github.com>
2024-10-23 19:55:05 +0530

    Update Card.js

--- Card.js
+++ Card.js
@@ -1,16 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Card(props) {
-  let [style, setStyle] = useState("none");
+  const { handleDelete, index, text, handleEdit, taskDone, done } = props;
+
+  let [edit, setEdit] = useState(false);
+  let [txt, setTxt] = useState(text);
+
+  useEffect(() => {
+    setTxt(text);
+  }, [text]);
+
+  function setEditText() {
+    setEdit(false);
+    handleEdit(index, txt);
+  }
+
+  function takeNewTask(e) {
+    setTxt(e.target.value);
+    console.log("takeNewTask");
+  }
   return (
-    <div className="task">
-      <p style={{ textDecoration: style }}>{props.text}</p>
-      <div className="imgs">
-        <div onClick={() => setStyle("line-through")} className="img">
-          <i className="fa-solid fa-check"></i>
+    <div className="task-card">
+      <div className="checked" onClick={() => taskDone(index)}>
+        {done ? (
+          <i className="fa-solid fa-square-check fa-xl"></i>
+        ) : (
+          <i className="fa-regular fa-square-check fa-xl"></i>
+        )}
+      </div>
+
+      <div className="text-area">
+        {edit ? (
+          <input
+            className="edit-task-input"
+            type="text"
+            value={txt}
+            onChange={takeNewTask}
+          />
+        ) : (
+          <div className="text">{txt}</div>
+        )}
+      </div>
+      <div className="icons">
+        <div className="delete" onClick={() => handleDelete(index)}>
+          <i className="fa-solid fa-trash"></i>
         </div>
-        <div onClick={() => setStyle("none")} className="img">
-          <i class="fa-solid fa-rotate-left"></i>
+        <div className="edit">
+          {edit ? (
+            <i className="fa-solid fa-check " onClick={setEditText}></i>
+          ) : (
+            <i
+              className="fa-solid fa-pen-to-square"
+              onClick={() => setEdit(true)}
+            ></i>
+          )}
         </div>
       </div>
     </div>
